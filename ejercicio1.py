882fbe012d9f29304e3ee04f63308ab32ea4f23f
luc1anobel <166134084+luc1anobel@users.noreply.github.com>
2024-10-31 11:39:06 -0300

    Add files via upload

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,23 @@
-'''1 -Clase para representar un Libro
-Crear una clase Libro que tenga las características título, autor y año de publicación. Del
-libro se debe poder obtener información, mostrando por mensaje todos sus datos. Se debe
-crear la clase e implementarla.'''
+'''1- Cargar secuencialmente una lista de alumnos, cada alumno
+debe ser un diccionario con cuatro claves: nombre, apellido, edad y
+la clave curso que posea de valor una tupla con el curso.'''
+
+# Lista para almacenar los alumnos 
+lista_de_alumnos = [] 
+def agregar_alumno():
+    res = "si"
+    while res == "si":
+        nombre = input("Ingrese el nombre del alumno: ") 
+        apellido = input("Ingrese el apellido del alumno: ") 
+        edad = int(input("Ingrese la edad del alumno: ")) 
+        curso = input("Ingrese la materia del alumno: ") 
+        curso = (curso, 117)
+        alumno = (f"nombre: {nombre}, apellido: {apellido}, edad: {edad}, curso: {curso}")
+        lista_de_alumnos.append(alumno) 
+        res = input("desea seguir ingresando alumnos(si-no) : ")
 
 
-class libro ():
-    def __init__ (self, titulo: str, autor: str, año_de_publicacion: int):
-        self.titulo = titulo
-        self.autor = autor
-        self.año_de_publicacion = año_de_publicacion
-    
-    def obtener_informacion (self):
-        print (f"el titulo del libro es: {self.titulo}\n el autor del libro es: {self.autor}\n el año en el que se publico es: {self.año_de_publicacion}")
-        return
-    
+agregar_alumno() 
 
+for alumno in lista_de_alumnos: 
+    print(alumno)
